e62d9478e6aae8f937e1858443f4e082362afe2d
JohanDenver <48187072+johandenver@users.noreply.github.com>
2019-05-01 23:06:43 -0700

    included updated js file with working video code, updated comments on .js file and .css file, updated the ReadMe file.

--- assets/app.js
+++ assets/app.js
@@ -1,19 +1,30 @@
 // create function to load page before .js runs:
 $(document).ready(function() {
-    //psuedocode:
 
-    //1. on click event: for user click on casino cards, dynamicaly create the content to append to the class "modal-body"
-
+    //click event: for user click on casino cards, dynamicaly create the content to append to the class "modal-body"
     $(document).on("click", ".card", function() {
         //captures the data name of the card"
         var venueId = $(this).attr("data-id");
         getUpcomingEvents(venueId);
     });
 
+    //click event: user click on Take a Look button (class=".show-video") to drop down the ".video-area" and call the playVideo function: 
+    $(document).on("click", ".show-video", function() {
+        //var divNumber = $(this).attr("id");
+        //console.log(divNumber);
+        $(this)
+            .siblings(".video-area")
+            .slideDown("slow");
+
+        var title = $(this).attr("data-name");
+        var buttonId = $(this).attr("id");
+        playVideo(title, buttonId);
+    });
+
+    // fucntion to pull the events API from Seatgeek, and dynamically create the events in the modal:
     function getUpcomingEvents(venueId) {
         // API Key: MTYzNjIxNzR8MTU1NjEyMjE2OC41OQ
         // Use ajax to pull data from the events API:
-
         var clientID = "MTYzNjIxNzR8MTU1NjEyMjE2OC41OQ";
 
         var queryUrl =
@@ -28,9 +39,7 @@ $(document).ready(function() {
         }).then(function(response) {
             //create variable name for the data response received from API:
             var eventResponse = response.events;
-            //log the resulting object
-            console.log(response);
-
+            // empty out the eventlist first:
             $(".events-list").empty();
 
             //loop through the response array to find the needed items:
@@ -39,41 +48,33 @@ $(document).ready(function() {
                 var date = response.events[i].datetime_local;
                 var tickets = response.events[i].url;
 
-                console.log(title);
-                console.log(date);
-                console.log(tickets);
-
                 // new div for events:
                 var eventDiv = $("<div>");
+                // add class:
                 eventDiv.addClass("container rounded event-container");
-                //create an event element:
+                // create an event elements for Title:
                 eventTitle = $("<h3>");
-                //attach text to the event element:
                 eventTitle.text("What's on: " + title);
-
+                 // create an event elements for Date:
                 eventDate = $("<p>");
                 eventDate.text("Date: " + date);
-
+                // create a link to buy tickets:
                 eventTickets = $("<a>").attr("href", tickets);
                 eventTickets.addClass("ticket-link");
                 eventTickets.attr("target", "_blank");
                 eventTickets.text("Buy Tickets");
-
+                // create a page break:
                 var br = $("<br>");
-
-                var videoButton = $("<button>");
-                // add a class to button variable:
+                // create a button to show the video:
+                var videoButton = $('<button id="'+ i +'" />');
                 videoButton.addClass("topic-btn rounded gif-button show-video");
-                // add an attribute to button variable:
                 videoButton.attr("data-name", title);
-                // add text to button variable:
                 videoButton.text("Take a look!");
 
-                // add div to contain the youtube video:
-                var videoBox = $("<div>");
-                // add class to the div for youtube video:
+                // add div to contain the youtube video, with different id's for each div created:
+                var videoBox = $('<div id="div' + i +'" />');
+                // add class to hide the div for youtube video:
                 videoBox.addClass("video-area hide");
-                videoBox.attr("id", "player");
 
                 // append the above variables to the eventDiv container:
                 eventDiv.append(
@@ -84,39 +85,39 @@ $(document).ready(function() {
                     videoButton,
                     videoBox
                 );
-                videoButton.on("click", function(event) {
-                    var videoName = $(this).attr("data-name");
-                    playVideo(videoName, videoButton, videoBox);
-                });
+
                 // append the eventDiv to the HTML class "events-list":
                 $(".events-list").append(eventDiv);
-                $(document).on("click", ".show-video", function() {
-                    $(this)
-                        .siblings(".video-area")
-                        .slideDown("slow");
-                });
+
             }
+
         });
+
     }
 
-    function playVideo(title, videoButton, videoBox) {
+    // function to pull the video from the YouTube:
+    function playVideo(title, buttonId) {
+        // Use ajax to pull data from the YouTube API:
         var videoQueryUrl =
             "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=" +
             title +
-            "&key=AIzaSyCGZCED449J3UvxPQRx_TbEpIi4nnesaZ8";
+            "&key=AIzaSyCQMj92WO36JhFdxY5j4sQvPXPL5ACyp-I";
+
         $.ajax({
             url: videoQueryUrl,
             method: "GET"
         }).then(function(response) {
+            //create variable name for the data response received from API:
             var YTID = response.items[0].id.videoId;
-            console.log(response);
-            console.log(YTID);
-            onYouTubeIframeAPIReady(YTID);
+            //call the YouTube iFrame function:
+            onYouTubeIframeAPIReady(YTID, buttonId);
         });
     }
 
-    function onYouTubeIframeAPIReady(YTID) {
-        player = new YT.Player("player", {
+    // function for the YouTube player: 
+    function onYouTubeIframeAPIReady(YTID, buttonId) {
+        console.log("div" + buttonId);
+        player = new YT.Player("div" + buttonId, {
             height: "40%",
             width: "100%",
             videoId: YTID,
@@ -124,17 +125,18 @@ $(document).ready(function() {
                 onReady: onPlayerReady,
                 onStateChange: onPlayerStateChange
             }
+        
         });
     }
 
-    // 4. The API will call this function when the video player is ready.
+    // The API will call this function when the video player is ready.
     function onPlayerReady(event) {
         event.target.playVideo();
     }
 
-    // 5. The API calls this function when the player's state changes.
-    //    The function indicates that when playing a video (state=1),
-    //    the player should play for six seconds and then stop.
+    // the API calls this function when the player's state changes.
+    // the function indicates that when playing a video (state=1),
+    // the player should play for six seconds and then stop.
     var done = false;
     function onPlayerStateChange(event) {
         if (event.data == YT.PlayerState.PLAYING && !done) {
@@ -142,7 +144,9 @@ $(document).ready(function() {
             done = true;
         }
     }
+
     function stopVideo() {
         player.stopVideo();
     }
+
 });
